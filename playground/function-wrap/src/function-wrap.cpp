059c09f05888fc23a7da1dc1fdec1276bc851356
Konstantyn Komarov <komarov.konstant@utexas.edu>
2017-06-14 20:06:30 -0500

    Added simple function wrapper example

---
+++ playground/function-wrap/src/function-wrap.cpp
@@ -0,0 +1,11 @@
+
+#include "jsbind11.h"
+
+double mul(double a, double b) {
+  return a * b;
+}
+
+JSBIND11_MODULE(addon, m) {
+  m.function("add", [](double a, double b) { return a + b; });
+  m.function("mul", &mul);
+};
---
+++ playground/function-wrap/test/test-function-wrap.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const funcWrap = require('../function-wrap.js');
+
+function testAdd()
+{
+    const a = 3;
+    const b = 7;
+
+    assert.strictEqual(funcWrap.add(a, b), a + b);
+}
+
+function testMul()
+{
+    const a = 3;
+    const b = 7;
+
+    assert.strictEqual(funcWrap.mul(a, b), a * b);
+}
+
+console.log("Tests passed- everything looks OK!");
